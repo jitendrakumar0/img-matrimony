db19d5610535f684c0eb94854840d8cf40b84d46
pooja <pooja1.img@gmail.com>
2023-02-08 17:10:53 +0530

    commit by pooja

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,4 +13,36 @@ $(window).scroll(function() {
     }
 });
 
+
+//? ###########################  Home Page Start  ###########################
+
+if ($("[page-name=serviceDetail]").length) {
+
+    var serviceOuterImg = document.querySelectorAll('.serviceOuterImg');
+    var serviceListImg = document.querySelectorAll('.serviceListImg');
+    let owlServiceDetail = document.querySelector('.owlServiceDetail');
+    
+    owlServiceDetail.addEventListener('click', (e) => {
+        let a = e.currentTarget;
+
+    })
+
+    // serviceListImg.forEach(btn => {
+    //     btn.addEventListener('click', (e) => {
+    //         var a = e.currentTarget.getAttribute('serviceList-Img');
+    //         var b = document.getElementById(a);
+    //         var c = b.parentElement;
+    //         c.forEach(img => {
+    //             img.classList.remove('active');
+    //         })
+    //         c.classList.add('active');
+    //         console.log(b);
+    //     })
+    // });
+
+    // serviceGalleryList.addEventListener('click', e => {
+        // var a = e.currentTarget.
+    // });
+}
+
 //Home page 
